17e946885f4b057c60b884bf06174ca9fa9100bb
gatherstead <gatherstead@gmail.com>
2025-07-09 19:46:11 -0400

    Add MailerLite global script to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,6 +48,14 @@ export default function RootLayout({
       lang="en"
       className={`${fraunces.variable} ${publicSans.variable} ${inter.variable} ${ibmPlexSans.variable}`}
     >
+      <Script
+  id="mailerlite"
+  strategy="beforeInteractive"
+  dangerouslySetInnerHTML={{
+    __html: `(function(w,d,e,u,f,l,n){w[f]=w[f]||function(){(w[f].q=w[f].q||[]).push(arguments);},l=d.createElement(e),l.async=1,l.src=u,n=d.getElementsByTagName(e)[0],n.parentNode.insertBefore(l,n);})(window,document,'script','https://assets.mailerlite.com/js/universal.js','ml');ml('account', '1653125');`,
+  }}
+/>
+
       <body className="antialiased bg-background text-text font-body">
         {children}
 
